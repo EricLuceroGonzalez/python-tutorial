e024e2f764b1e8ddbddb9f05ac9f6953d1a6dca9
Ericlucerogonzalez <35483789+EricLuceroGonzalez@users.noreply.github.com>
2023-11-22 15:17:57 -0500

    Update pymunk-1.py

--- pymunk-1.py
+++ pymunk-1.py
@@ -1,57 +1,49 @@
-# pymunk-1.py
-"""Very simple example that does not depend on any third party library such 
-as pygame or pyglet like the other examples. 
-"""
-
-import random
-import sys
-
+# pymunk-4.py
+# https://www.youtube.com/watch?v=tLsi2DeUsak
+#? (0) Importar librerias necesarias
+import pygame
 import pymunk
-import pymunk.util
-from pymunk import Vec2d
+import pymunk.pygame_util
+import math
+
+#? (1) Iniciar todo
+pygame.init()
+ANCHO, ALTO = 1000, 750
+window = pygame.display.set_mode((ANCHO, ALTO))
+pygame.display.set_caption("PYGAME: Simulación")
 
 
-def main():
-    print(f"basic example of pymunk {pymunk.version}")
+#? (2) 
+def run(window, ancho, alto):
+    run = True
+    clock = pygame.time.Clock()
+    fps = 60
+    dt = 1/fps
+
+    # ! CREAR ESPACIO
     space = pymunk.Space()
-    space.gravity = (0.0, -900.0)
+    space.gravity = (0, 981); # * Define la gravedad en x,y (y aumenta hacia abajo)
 
-    ## Balls
-    balls = []
+    draw_options = pymunk.pygame_util.DrawOptions(window)
 
-    ticks_to_next_ball = 10
-    for x in range(5000):
-        ticks_to_next_ball -= 1
-        if ticks_to_next_ball <= 0:
-            ticks_to_next_ball = 10000
-            mass = 10
-            radius = 25
-            inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
-            body = pymunk.Body(mass, inertia)
-            x = random.randint(115, 350)
-            body.position = x, 400
-            shape = pymunk.Circle(body, radius, Vec2d(0, 0))
-            space.add(body, shape)
-            balls.append(shape)
+    while run:
+        for event in pygame.event.get():  # Check events running
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        draw(space, window, draw_options)
+        space.step(dt) # Rapidez de la simulación
+        clock.tick(fps)
 
-        balls_to_remove = []
+    pygame.quit()
 
-        for ball in balls:
-            if ball.body.position.y < 0:
-                balls_to_remove.append(ball)
-        for ball in balls_to_remove:
-            space.remove(ball, ball.body)
-            balls.remove(ball)
 
-        if len(balls) >= 1:
-            v = balls[0].body.position
-            print("(in on_draw): point = %.2f, %.2f" % (v.x, v.y))
-
-        ### Update physics
-        for x in range(1):
-            space.step(1 / 50.0)
-    print("done!")
-
+# ?(3) Draw funcion
+def draw(space, window, draw_options):
+    window.fill("white")
+    space.debug_draw(draw_options)
+    pygame.display.update()
 
 if __name__ == "__main__":
-    sys.exit(main())
+    run(window, ANCHO, ALTO)
+
